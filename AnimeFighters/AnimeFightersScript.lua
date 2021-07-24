93f3cb781bb6139c649e107ff0aa7e8ab69a8a0e
Superflare9 <riptide12452@gmail.com>
2021-07-23 17:19:52 -0700

    Update AnimeFightersScript.lua
    Added
    -AntiAfk

--- AnimeFighters/AnimeFightersScript.lua
+++ AnimeFighters/AnimeFightersScript.lua
@@ -196,3 +196,15 @@ Page3.Button({
 		end)
 	end
 })
+
+Page3.Button({
+	Text = "AntiAFK",
+	 Callback = function()
+		local vu = game:GetService("VirtualUser")
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
+   			vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+  			wait(1)
+   		vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	end) 
+end
+})
